460b93ae7279a91b0b6abc3b24788e2eaaadf81c
nekoy3 <nekoyama3.n@gmail.com>
2021-07-29 23:38:10 +0900

    形式変更
    問題作成コストを軽減

--- quiz.py
+++ quiz.py
@@ -1,5 +1,6 @@
-#入力形式(テキストファイル）　問題文 選択肢1/選択肢2/選択肢3/選択肢4 答えの選択肢の番号 解説
+#入力形式(テキストファイル）　問題文 選択肢1 選択肢2 選択肢3 選択肢4 解説
 #ファイルを読み込む→範囲内の行からランダムで行を選ぶ→問題、選択肢、解説を選択→処理
+#選択肢の末尾に*を入れることで、それが答えと判定する。
 import sys
 import os
 import random
@@ -26,19 +27,42 @@ qSuc = qCnt = 0
 while True:
     useLine = random.randint(0, len(beforeText)-1)
     line = beforeText[useLine].rstrip().split(' ')
-    what = ' ' + line[1].replace('/',' ')
-    for i in range(1,5):
-        after = '　' + str(i) + ','
-        what = what.replace(' ',after,1)
+    what = ''
+    ansCnt = 0
+    for i in range(4):
+        try:
+            line[i+1].index('*')
+        except ValueError:
+            line[i+1] = line[i+1]
+        else:
+            line[i+1] = line[i+1].replace('*','')
+            ansCnt = i + 1
+        what += ' ' + str(i+1) + ',' + line[i+1]
+    if ansCnt == 0:
+        print('問題「' + line[0] +'」' + 'には答えが設定されていません。')
+        continue
+    
     print("Question : " + line[0] + '\n' + what)
-    print("回答 ：",end=" ")
-    answer = input()
-    if answer == line[2]:
-        print('正解！答えは ' + line[2] + ' です。　解説：' + line[3])
+    answer = ''
+    while answer == '':
+        print("回答 ：",end=" ")
+        answer = input()
+    if int(answer) == ansCnt:
+        try:
+            line[5]
+        except:
+            print('正解！答えは ' + line[ansCnt] + ' です。')
+        else:
+            print('正解！答えは ' + line[ansCnt] + ' です。　解説：' + line[5])
         qCnt += 1
         qSuc += 1
     else:
-        print('不正解！　解説：' + line[3])
+        try:
+            line[5]
+        except:
+            print('不正解。答えは ' + line[ansCnt] + ' です。')
+        else:
+            print('不正解。答えは ' + line[ansCnt] + ' です。　解説：' + line[5])
         qCnt += 1
 
     print("\n続けるにはEnterを押してください・・・\n終了する場合はendを入力してください・・・")
