ce861e0608f331d118cd7d82331a058125aeaa58
nekoy3 <nekoyama3.n@gmail.com>
2021-08-21 21:34:18 +0900

    不要ファイル削除

--- Untitled-1.py
+++
@@ -1,7 +0,0 @@
-import re
-exeCmd = 0
-cmdLine = 'execute @a[scores={sn=5,test=17..83},tag=select] ~ ~ ~ summon zombie ~ ~ ~'
-
-cmdLine2 = '@a[scores={sn=5,test=17..83}'
-exeCmd = re.search(r'\{.*\,.*\}',cmdLine2).group(0)
-print(exeCmd)
