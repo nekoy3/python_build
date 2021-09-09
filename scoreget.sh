be70c0e7b52b2000000d0c47f3e7f7b316741349
nekoy3 <nekoyama3.n@gmail.com>
2021-09-10 00:52:28 +0900

    色付けと一部修正
    color指定するとargumentが多すぎるだの、不明なオプションだのエラーが出てくるのでコンソールでのみ利用できるカラーコードで代用

--- scoreget.sh
+++ scoreget.sh
@@ -5,13 +5,15 @@ n=' '
 while (true); do
     log=`tail -n 2 logs/latest.log`
     if [ "`echo $log | grep "issued server command: /w @s ランキング参照"`" ]; then
-        screen -p 0 -S aziServer -X eval 'stuff "save-all\015"'
         name=`echo $log | sed -r 's/ issued server command: \/w @s ランキング参照//g' | rev | awk '{ print $1 }' | rev`
-        screen -p 0 -S aziServer -X eval 'stuff "tellraw '$name' {"'
-        screen -p 0 -S aziServer -X stuff \"text\":\"--------------------スコアランキング--------------------\"}\\015
-        #screen -p 0 -S aziServer -X eval 'stuff "\015"'
+        screen -p 0 -S aziServer -X eval 'stuff "save-all\015"'
+        sleep 1
+        screen -p 0 -S aziServer -X eval 'stuff "tellraw '$name' ["'
+        screen -p 0 -S aziServer -X stuff \"\",{\"text\":\"§b--------------------\"},
+        screen -p 0 -S aziServer -X stuff {\"text\":\"§aスコアランキング\"},{\"text\":\"§b--------------------\"}]
+        screen -p 0 -S aziServer -X eval 'stuff "\015"'
         #ファイルを読み込む
-        python nbt_edit.py ./azi_server/data/scoreboard.dat 1,totalScoreXpl,t export
+        python nbt_edit.py ./azi_server/data/scoreboard.dat 1,totalScoreXpl,t export > /dev/null
         text='./azi_server/data/export.txt'
         #区切り文字を改行に指定
         IFS=$'\n'
@@ -20,18 +22,33 @@ while (true); do
         rank=0
         for s in "${file[@]}"; do
             rank=$((rank += 1))
-            text=`echo \#${rank} - ${s/Name\:}`
-            text=${text/Obj\:totalScoreXpl  }
-            if [ `echo $text|grep ${name}` ]; then
-                my=${text}
-            fi
+            text=`echo §l§8\#${rank} §r- §6${s/Name\:}`
+            text=${text//Obj\:totalScoreXpl }
+            text=${text// Score:/ §7Score:§c§o}
             if [ $rank -le 10 ]; then
                 screen -p 0 -S aziServer -X eval 'stuff "tellraw '${name}' {"'
-                screen -p 0 -S aziServer -X stuff \"text\":\"${text}\"}\\015
+                if [ $rank -eq 1 ]; then
+                    text=${text//§8\#${rank}/§4\#${rank}}
+                    screen -p 0 -S aziServer -X stuff \"text\":\"${text}\"}\\015
+                fi
+                if [ $rank -eq 2 ]; then
+                    text=${text//§8\#${rank}/§5\#${rank}}
+                    screen -p 0 -S aziServer -X stuff \"text\":\"${text}\"}\\015
+                fi
+                if [ $rank -eq 3 ]; then
+                    text=${text//§8\#${rank}/§9\#${rank}}
+                    screen -p 0 -S aziServer -X stuff \"text\":\"${text}\"}\\015
+                fi
+                if [ $rank -ge 4 ]; then
+                    screen -p 0 -S aziServer -X stuff \"text\":\"${text}\"}\\015
+                fi
+                if [ `echo $text|grep ${name}` ]; then
+                    my=${text}
+                fi
             fi
         done
         screen -p 0 -S aziServer -X eval 'stuff "tellraw '$name' {"'
-        screen -p 0 -S aziServer -X stuff \"text\":\"----------------------------------------\"}\\015
+        screen -p 0 -S aziServer -X stuff \"text\":\"§b----------------------------------------------------\"}\\015
         screen -p 0 -S aziServer -X eval 'stuff "tellraw '${name}' {"'
         screen -p 0 -S aziServer -X stuff \"text\":\"${my}\"}\\015
         screen -p 0 -S aziServer -X eval 'stuff "w @s a\015"'
@@ -40,3 +57,4 @@ while (true); do
     sleep 1
 done
 #issued server command: /w @s ランキング参照
+#sudo kill `ps aux | grep 'scoreget' | awk '{ print $2 }'`
