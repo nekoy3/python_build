c74ea881683429da73c1371691396cb6a5cfed1e
nekoy3 <nekoyama3.n@gmail.com>
2021-10-21 13:06:41 +0900

    ファイル読み書き関連の記述を修正

--- your_burden.py
+++ your_burden.py
@@ -1,10 +1,5 @@
 # coding: utf_8
-#入力想定(引数)
 #操作ファイルはburden.txtファイル固定にアクセスするものとする。無ければ生成し通知する。
-#データの追加：add <科目> <内容> <提出期限yyyy/mm/dd,hh:mm> <提出先リンク> また、固有番号をそれぞれに付与する。
-#データの参照：w <参照タイプ> <sort> まだ期限じゃないもの：yet 期限過ぎた物：old <いつ以降yyyy/mm/dd,hh:mm>※未記入or[all]は全て参照、50件超える場合はエラー　固有番号も表示する
-#<sort>は 期限：time 科目名：subj trueが昇順,falseが降順(一番上が一番大きい数字になる)
-#データの削除：rme <固有番号> ただしremovedタグを付与するのみ
 import datetime
 import PySimpleGUI as sg
 import os
@@ -35,17 +30,25 @@ else:
 layout = [  [sg.Text('大学からのburdenを管理します。')],
             [sg.Text('操作', size=(3, 1)),sg.Combo(('課題を追加する', '現実を見る（課題を閲覧する）', '存在しない（追加ミス）課題を玉砕する', '完了フラグを付ける'), default_value="課題を追加する",size=(35, 1), key='cmd')],
             [sg.Text('※時間指定について 時間指定(24h)は出来ますが、分、秒単位は無視されます。')],
-            #[sg.Input(key='-Input-'), sg.CalendarButton('calender', target='-Input-')],
+            [sg.Input(key='-Input-'), sg.CalendarButton('calender', target='-Input-')],
             [sg.Button('OK'), sg.Button('終了')] ]
 
 window = sg.Window('your_burden', layout)
 
 def writeline_file(text):
+    with open('./burden.txt', 'a', encoding='UTF-8') as f:
+        f.write(text + "\n")
 
-    outText = open('./burden.txt', 'a', encoding='UTF-8')
-    outText.write(text + "\n")
-    outText.close
-
+def readFile():
+    with open('./burden.txt', 'r', encoding='UTF-8') as f:
+        data = list(f)
+    delList = []
+    for i in range(len(data)):
+        if data[i] == "#REMOVED!!\n":
+            delList.append(i)
+    for i in list(reversed(delList)):
+        data.pop(i)
+    return data
 
 def data_add():
 
@@ -69,8 +72,6 @@ def data_add():
                 with open('./burden.txt') as f:
                     totalLine = sum(1 for line in f)
             except:
-                f = open('./burden.txt', 'w')
-                f.close()
                 totalLine = 0
 
             if values['subject'] == "" or values['subjectInfo'] == "" or values['kigen'] == "":
@@ -123,7 +124,7 @@ def data_select():
         [sg.Text("課題を検索します。")],
         [sg.Text('操作', size=(3, 1)),sg.Combo(('期限が迫っている未完了の課題を表示する', '期限過ぎているのも含め未完了の課題を表示する', '期限がまだあるが、完了した課題を見て悦に浸る', '期限が過ぎた、絶望の課題','指定日時以降が期限の課題'), default_value="期限が迫っている未完了の課題を表示する",size=(35, 1), key='cmd')],
         [sg.Text("提出期限(yyyy/mm/dd 時:分:秒)"),sg.CalendarButton('calender', target='kigen'),sg.Input(key='kigen',size=(20, 1))],
-        [sg.Output(size=(50,10), key='-OUTPUT-')],
+        #[sg.Output(size=(50,10), key='-OUTPUT-')],
         [sg.Button("検索", size=(10, 1)),sg.Button("キャンセル", size=(10, 1))]]
     
     main_window = sg.Window("課題を追加する", main_layout)
@@ -139,34 +140,34 @@ def data_select():
 
             main_window['-OUTPUT-'].update('')
 
-            with open('./burden.txt') as f:
-                allData = list(f)
+            file = readFile()
 
-            for i in range(len(allData)):
-                allData[i] = allData[i].split('//')
-                allData[i][3] = datetime.datetime.strptime(allData[i][3], '%Y-%m-%d %H:%M:%W')
+            for i in range(len(file)):
+                file[i] = file[i].split('//')
+                print(file)
+                file[i][3] = datetime.datetime.strptime(file[i][3], '%Y-%m-%d %H:%M:%W')
 
             dt_now = datetime.datetime.now()
 
             if values['cmd'] == '期限が迫っている未完了の課題を表示する':
-                for data in allData:
+                for data in file:
                     if dt_now <= data[3]:
                         condition = "未完了" if data[4] == "uncompleted\n" else "完了"
                         print("-----------------\nNUM:" + data[0] + "\n科目名:" + data[1] + "\n内容:" + data[2] + "\n提出期限:" + str(data[3]) + "\n状態:" + condition)
 
             elif values['cmd'] == '期限過ぎているのも含め未完了の課題を表示する':
-                for data in allData:
+                for data in file:
                     if data[4] == "uncompleted\n":
                         condition = "未完了"
                         print("-----------------\nNUM:" + data[0] + "\n科目名:" + data[1] + "\n内容:" + data[2] + "\n提出期限:" + str(data[3]) + "\n状態:" + condition)
 
             elif values["cmd"] == '期限がまだあるが、完了した課題を見て悦に浸る':
-                for data in allData:
+                for data in file:
                     if dt_now <= data[3] and data[4] == "completed\n":
                         print("-----------------\nNUM:" + data[0] + "\n科目名:" + data[1] + "\n内容:" + data[2] + "\n提出期限:" + str(data[3]) + "\n状態:" + condition)
 
             elif values["cmd"] == '期限が過ぎた、絶望の課題':
-                for data in allData:
+                for data in file:
                     if dt_now >= data[3] and data[4] == "uncompleted\n":
                         print("-----------------\nNUM:" + data[0] + "\n科目名:" + data[1] + "\n内容:" + data[2] + "\n提出期限:" + str(data[3]) + "\n状態:" + condition)
 
@@ -176,7 +177,7 @@ def data_select():
                 except:
                     print("エラー：時刻指定が正しくありません。")
                     continue
-                for data in allData:
+                for data in file:
                     if set_dt <= data[3]:
                         condition = "未完了" if data[4] == "uncompleted\n" else "完了"
                         print("-----------------\nNUM:" + data[0] + "\n科目名:" + data[1] + "\n内容:" + data[2] + "\n提出期限:" + str(data[3]) + "\n状態:" + condition)
@@ -198,23 +199,21 @@ def data_remove():
             break
 
         elif event == "削除":
-            with open('./burden.txt') as f:
-                allData = list(f)
+            file = readFile()
             main_window['-OUTPUT-'].update('')
-            for i in range(len(allData)):
-                burNum = allData[i].split('//')[0]
+            for i in range(len(file)):
+                burNum = file[i].split('//')[0]
                 if burNum == values['removeNum']:
-                    print(allData[i] + "削除しました。")
-                    allData[i] = "#REMOVED!!"
-                    allDataStr = ""
+                    print(file[i] + "削除しました。")
+                    file[i] = "#REMOVED!!\n"
+                    fileStr = ""
+                    for i in file:
+                        fileStr += i
                     with open('./burden.txt', mode='w') as f:
-                        for i in allData:
-                            allDataStr += i
-                        f.write(allDataStr)
+                        f.write(fileStr)
                     break
             else:
                 print("入力値が正しくありません。")
-            #ここで番号が入力値と適合した行を#REMOVED!に置き換える。行数を減らすと番号振り当て時に不都合が発生する。
 
 def data_changeflag():
     exit()
