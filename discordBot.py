d84076e22897b1aab6ea489ad3b42d59614d24e9
nekoy3 <nekoyama3.n@gmail.com>
2021-12-13 22:37:27 +0900

    データベース関連の処理をすこし

--- discordBot.py
+++ discordBot.py
@@ -1,12 +1,64 @@
 #py -3 -m pip install -U discord.py
-
+# インストールした discord.py を読み込む
 import discord
-from discord.ext import commands
+import sqlite3
+import os
 
-bot = commands.Bot(command_prefix='>')
+dbname = "datalist.db"
+if os.path.exists(dbname):
+    print("データベースファイルを認識しました。起動中はファイルを操作しないでください。")
+else:
+    conn = sqlite3.connect(dbname)
+    # SQLiteを操作するためのカーソルを作成
+    cur = conn.cursor()
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    # テーブルの作成
+    cur.execute(
+        'CREATE TABLE items(id INTEGER PRIMARY KEY AUTOINCREMENT,user_id STRING, name STRING)'
+    )
+    conn.close()
+    print("データベースファイルとテーブルを新規に作成しました。起動中はファイルを操作しないでください。")
 
-bot.run('ODg2OTgwOTgwOTY2MDYwMDQy.YT9faQ.mp5kjBS5QiOBzdrmyZXEzqx_BEc')
+#カラムの追加
+def column_add(s,msg):
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+    cur.execute(
+        "ALTER TABLE items ADD COLUMN %s STRING"
+     %s)
+    cur.execute(
+        'PRAGMA table_info(items)'
+    )
+    return cur.fetchall()
+
+#ユーザー情報の追加
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'token' #pushする前にトークンを削除
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+    #  /addcolumn A テーブルに「A」というカラムを追加する。
+    if message.content.startswith('/addcolumn'):
+        columnName = message.content.split(' ')[1]
+        text = column_add(columnName,message)
+        await message.channel.send(text)
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
