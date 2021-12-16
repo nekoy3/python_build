22f499f6621e065db67b772f318b0cbc4678d03e
nekoy3 <nekoyama3.n@gmail.com>
2021-12-16 13:32:47 +0900

    コマンドの追加 (ebot v1.0.0)
    リリース版（ただしデータベースは一つのみ）
    mycolumnedit 自身のカラムの情報を編集する
    bomb ○秒後に削除されるメッセージを送信する
    dice n個のm面サイコロを振る
    ping ping値を測定する
    update アプデ情報を表示する
    invite 招待リンクを表示

--- discordBot.py
+++ discordBot.py
@@ -1,7 +1,10 @@
+# encoding: utf-8
 #py -3 -m pip install -U discord.py
 # インストールした discord.py を読み込む
 import discord
+import asyncio
 import sqlite3
+import random
 import os
 
 #ユーザー情報の追加
@@ -11,6 +14,7 @@ client = discord.Client()
 
 # 自分のBotのアクセストークンに置き換えてください
 TOKEN = 'token' #pushする前にトークンを削除
+version = "v1.0.0"
 
 dbname = "datalist.db"
 if os.path.exists(dbname):
@@ -33,6 +37,11 @@ else:
     conn.close()
     print("データベースファイルとテーブルを新規に作成しました。起動中はファイルを操作しないでください。")
 
+#asyncで引数sec秒待機してmsgを削除する
+async def wait_sec_and_delete_message(sec, msg):
+    await asyncio.sleep(sec)
+    await msg.delete()
+    return True
 
 #初期設定が行われているかを判別する（一人でもフィールドがあればTrue）
 def is_init():
@@ -54,10 +63,19 @@ def break_not_operator_command(uid):
     cur.execute("SELECT operator FROM items WHERE user_id = '%s'" %uid)
     operator = cur.fetchone()
     conn.close()
-    if operator[0] == 1:
-        return True
-    else:
+    try:
+        if operator[0] == 1:
+            return True
+        else:
+            return False
+    except TypeError:
         return False
+
+#idからユーザー名を取得する
+async def id_to_username(id):
+    u_id = await client.fetch_user(id)
+    uname = str(u_id)
+    return uname
 
 #embedメッセージ生成用メソッド
 #タイトル、descriptionを引数に設定する
@@ -70,7 +88,7 @@ def add_embed(title,descrip):
     embed.set_author(name=client.user, # Botのユーザー名
                      icon_url=client.user.avatar_url # Botのアイコンを設定してみる
                      )
-    embed.set_footer(text="Powered by nekoy3")
+    embed.set_footer(text="Powered by nekoy3 " + version)
     return embed
 
 #管理者権限でのみ可能な処理実行後のembed(色がマゼンタ 0xc71585)
@@ -83,7 +101,7 @@ def add_embed_special(title,descrip):
     embed.set_author(name=client.user, # Botのユーザー名
                      icon_url=client.user.avatar_url # Botのアイコンを設定してみる
                      )
-    embed.set_footer(text="Powered by nekoy3")
+    embed.set_footer(text="Powered by nekoy3" + version)
     return embed
 
 #初期設定で呼び出すメソッド。add_userの機能に加えてoperatorフィールドをtrueにする
@@ -110,7 +128,7 @@ def add_embed_failed(title,descrip):
     embed.set_author(name=client.user, # Botのユーザー名
                      icon_url=client.user.avatar_url # Botのアイコンを設定してみる
                      )
-    embed.set_footer(text="Powered by nekoy3")
+    embed.set_footer(text="Powered by nekoy3" + version)
     return embed
 
 
@@ -174,7 +192,7 @@ def user_add(user):
      %user[0])
     if len(cur.fetchall()) == 0: #返り値が存在しない=該当データが存在しない時
         cur.execute(
-            "INSERT INTO items ( user_id, user_name ) values ( ? , ? )",
+            "INSERT INTO items ( user_id, user_name, operator ) values ( ? , ? , ? )",
         user)
         conn.commit()
         callmsg = "ユーザID:" + str(user[0]) + "\nユーザ名:" + user[1]
@@ -197,23 +215,28 @@ def user_info(name):
     cur.execute(
         "SELECT * FROM items WHERE user_name = '%s'"
     %name)
-    user = cur.fetchall()
-    if len(user) == 0:
+    try:
+        user = list(cur.fetchone())
+    except:
         callmsg = "ユーザ名:" + name + "は登録されていません。"
         embed = add_embed_failed("ユーザ名が見つかりません。", callmsg)
-    else: #user[0]に検索結果のイテレータが存在する [0][1]はid,[0][2]はname。
+    else: #userに検索結果のイテレータが存在する [1]はid,[2]はname。
         callmsg = ""
         cur.execute(
             'PRAGMA table_info(items)'
         )
         columnList = []
         fetchList = cur.fetchall()
-        for i in range(len(fetchList)-2):
+        for i in range(len(fetchList)):
             #イテレータからカラム名のみを取得しリストに追加
-            columnList.append(fetchList[i+2][1])
-        for i in range(len(columnList)):
-            callmsg += columnList[i] + ":" + str(user[0][i+2]) + "\n"
-        embed = add_embed("ユーザID:" + str(user[0][0]) + "\nユーザ名:" + user[0][1] + "についての情報", callmsg)
+            columnList.append(fetchList[i][1])
+        embed = add_embed("ユーザ名:" + user[1] + "についての情報", "一覧形式で表示します。")
+        del user[0:3]
+        del columnList[0:3]
+        for i in range(len(user)):
+            embed.add_field(name=columnList[i], value=user[i], inline=False)
+        embed.set_footer(text="Powered by nekoy3")
+        return embed
     conn.close()
     return embed
 
@@ -230,12 +253,30 @@ def column_edit(columnName, string):
     conn.close()
     return embed
 
+#descriptionレコードでは無くuser_name=unameのカラムのフィールドに挿入する
+def my_column_edit(uname, columnName, string):
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+    cur.execute(
+        "UPDATE items SET %s = '%s' WHERE user_name = '%s'"
+    %(columnName, string, uname))
+    conn.commit()
+    callmsg = columnName + "を「" + string + "」に変更しました。"
+    embed = add_embed("レコード操作", callmsg)
+    conn.close()
+    return embed
+
 #SQL文を実行する
 def sql_execute(sql):
     conn = sqlite3.connect(dbname)
     cur = conn.cursor()
-    cur.execute(sql)
-    callmsg = "SQL文を実行しました。\n" + sql + "\n" + cur.fetchall()
+    sqlAfter = sql.replace("__", "8888UNDERBAR8888").replace("_"," ").replace("8888UNDERBAR8888","_") 
+    try:
+        cur.execute(sqlAfter)
+    except sqlite3.OperationalError:
+        embed = add_embed_failed("SQL文が不正です。", sqlAfter)
+        return embed
+    callmsg = "SQL文を実行しました。\n" + sqlAfter + "\n\n↓ RETURN RESULT ↓\n" + str(cur.fetchall())
     conn.commit()
     conn.close()
     embed = add_embed_special("SQL文実行", callmsg)
@@ -253,11 +294,16 @@ def help():
     embed.add_field(name=">columnedit <カラム名> <変更内容>",value="カラムの説明文を設定します。（権限者専用）", inline=False)
     embed.add_field(name=">mycolumnedit <カラム名> <変更内容>",value="自身のカラム(ゲーム情報)を設定します。", inline=False)
     embed.add_field(name=">removecolumn <カラム名>",value="特定のカラムを削除します。（権限者専用）\n※データも削除されるので注意してください。", inline=False)
-    embed.add_field(name=">SQL <SQL文>",value="SQL文を実行します。（権限者専用）\n>sqhelp で詳細を表示", inline=False)
-    embed.add_field(name=">stop",value="botを終了します。", inline=False)
+    embed.add_field(name=">SQL <SQL文>",value="SQL文を実行します。（権限者専用）\n>sqhelp で詳細を表示\n空白はアンダーバー挿入で認識(アンダーバーを入れる場合は２度入力)", inline=False)
+    embed.add_field(name=">stop",value="botを終了します。（権限者専用）", inline=False)
     embed.add_field(name=">neko",value="にゃ～ん", inline=False)
     embed.add_field(name=">columnlist",value="カラム一覧を表示します。", inline=False)
-    embed.set_footer(text="Powered by nekoy3")
+    embed.add_field(name=">bomb",value="「bomb 秒 メッセージ」bombを実行します。botがメッセージを送信して秒後にメッセージを削除します。", inline=False)
+    embed.add_field(name=">dice",value="「dice 個数d面数」面数サイコロを個数回投げます。何も入力しなければ六面サイコロを２個投げます。", inline=False)
+    embed.add_field(name=">ping",value="botのpingを表示します。", inline=False)
+    embed.add_field(name=">invite",value="botのinviteリンクを表示します。", inline=False)
+    embed.add_field(name=">update",value="サーバーの更新情報を表示します。", inline=False)
+    embed.set_footer(text="Powered by nekoy3" + version)
     return embed
 
 #チュートリアルを返す
@@ -268,7 +314,7 @@ def tutorial():
     embed.add_field(name="②自分の情報(ゲームのフレンドコードやID)を登録する",value="「>mycolumnedit カラム名(ゲーム名) 自身の情報」で登録することができます。", inline=False)
     embed.add_field(name="③他者の情報を参照する",value="「>info ユーザー名」で他者の情報を参照することができます。\nユーザー名を入力しない場合、自身の情報を参照します。", inline=False)
     embed.add_field(name="ゲーム項目（カラム）の追加",value="権限者が「>addcolumn カラム名」を実行するだけで追加できるので、追加してもらうようにしましょう。\n「>columnlist」で現在登録できるカラムを参照できます。", inline=False)
-    embed.set_footer(text="Powered by nekoy3")
+    embed.set_footer(text="Powered by nekoy3" + version)
     return embed
 
 #カラム一覧を返す(PRAGMA)
@@ -300,6 +346,13 @@ def column_list():
         embed.add_field(name=colNameList[i],value=descNameList[i], inline=False)
     return embed
 
+#n,mを引き渡してサイコロを振る
+def dice(n,m):
+    countList = []
+    for i in range(n):
+        countList.append(random.randint(1,m))
+    return countList
+
 # 起動時に動作する処理
 @client.event
 async def on_ready():
@@ -329,6 +382,7 @@ async def on_message(message):
     # 「/neko」と発言したら「にゃーん」が返る処理
     elif message.content == '>neko':
         await message.channel.send('にゃーん')
+        await message.delete()
 
     #  /addcolumn A テーブルに「A」というカラムを追加する。
     elif message.content.startswith('>addcolumn'):
@@ -350,6 +404,7 @@ async def on_message(message):
         user = [message.author.id]
         u_id = await client.fetch_user(user[0])
         user.append(str(u_id))
+        user.append(False) #オペレーター権限がfalseである意
         embed = user_add(user)
         await message.channel.send(embed=embed)
 
@@ -371,8 +426,10 @@ async def on_message(message):
             user = message.author.id
             u_id = await client.fetch_user(user)
             info = str(u_id)
-        if info.startswith('@'):
-            info = info.replace('@','')
+        #infoに@を含む場合raw_mentionsでidを取得する
+        if info.startswith('<@') and info.endswith('>'):
+            user = message.raw_mentions[0]
+            info = await id_to_username(user)
         embed = user_info(info)
         await message.channel.send(embed=embed)
     
@@ -435,6 +492,75 @@ async def on_message(message):
         embed = column_delete(columnName)
         await message.channel.send(embed=embed)
     
+    #自身のカラムを編集する。 columnedit
+    elif message.content.startswith('>mycolumnedit'):
+        info = id_to_username(message.author.id)
+        try:
+            columnName = message.content.split(' ')[1]
+        except:
+            await message.channel.send("カラム名を入力して実行してください。")
+            return
+        try:
+            s = message.content.split(' ')[2]
+        except:
+            await message.channel.send("情報を入力して実行してください。")
+            return
+        #my_column_edit(uname, columnName, string)
+        embed = my_column_edit(info, columnName, s)
+        await message.channel.send(embed=embed)
+    
+    # >bomb <秒数> <内容> 秒後に削除するメッセージを送信する
+    elif message.content.startswith('>bomb'):
+        try:
+            sec = int(message.content.split(' ')[1])
+        except:
+            await message.channel.send("秒数を入力して実行してください。")
+            return
+        try:
+            content = message.content.split(' ')[2]
+        except:
+            await message.channel.send("内容を入力して実行してください。")
+            return
+        name = await id_to_username(message.author.id)
+        await message.delete()
+        msg = await message.channel.send(name + "がbombを送信しました。：\n" + content)
+        await wait_sec_and_delete_message(sec, msg)
+    
+    # >dice nDm m面サイコロをn個振る。
+    elif message.content.startswith('>dice'):
+        msg = message.content.lower()
+        try:
+            n,m = int(msg.split(' ')[1].split('d')[0]), int(msg.split(' ')[1].split('d')[1])
+        except:
+            n = 2
+            m = 6
+        
+        if n > 100:
+            await message.channel.send("サイコロの数は100までです。")
+            return
+        if m > 100:
+            await message.channel.send("サイコロの面の数は100までです。")
+            return
+        countList = dice(n,m)
+        embed = add_embed("サイコロを振りました。", str(m) + "面サイコロを" + str(n) + "個振りました。\n " + str(countList) + "\n" + "合計値は" + str(sum(countList)) + "です。\nMAX=" + str(n*m) + " MIN=" + str(n))
+        await message.channel.send(embed=embed)
+
+    # >ping ping値を取得する
+    elif message.content.startswith('>ping'):
+        embed = add_embed("ping値を取得しました。", "ping値は" + str(round(client.latency * 1000,1)) + "ミリ秒です。")
+        await message.channel.send(embed=embed)
+    
+    # >update このbotの更新情報を取得する。
+    elif message.content.startswith('>update'):
+        embed = add_embed("Update log", "このbotは" + str(version) + "です。")
+        embed.add_field(name="v1.0.0('21/12/16)", value="初回リリース版", inline=False)
+        await message.channel.send(embed=embed)
+    
+    # >invite このbotの招待リンクを取得する。
+    elif message.content.startswith('>invite'):
+        embed = add_embed("Invite link", "このbotを招待するにはこちらのリンクをクリックしてください。\nhttps://discord.com/api/oauth2/authorize?client_id=886980980966060042&permissions=19260894449&scope=bot")
+        await message.channel.send(embed=embed)
+
     # コマンドに該当しなかったときにエラーを返す
     else:
         await message.channel.send("コマンドが正しくありません。\n>helpでコマンド一覧を確認できます。")
